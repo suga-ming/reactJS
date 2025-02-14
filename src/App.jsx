98c9a305b13e4a6c942af1705e76799d135226c8
suga-ming <kby9901@naver.com>
2025-02-14 17:17:40 +0900

    RockSiccorsPaper

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./App.css";
+import Box from "./components/Box";
+
+const choice = {
+  r: {
+    name: "Rock",
+    img: "https://blog.kakaocdn.net/dn/pSJwo/btqXJV1lACE/nx5XrxkCLWXh9UsnoS8vbK/img.png",
+  },
+  s: {
+    name: "Scissors",
+    img: "https://blog.kakaocdn.net/dn/HfURw/btqXKvOTNWK/gWTwPXEg9QzSV0ilOuwuak/img.png",
+  },
+  p: {
+    name: "Paper",
+    img: "https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png",
+  },
+};
+
+function App() {
+  const [select, setSelect] = useState(null);
+  const [computer, setComputer] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const play = (userchoice) => {
+    setSelect(choice[userchoice]);
+    let computerChoice = randomChoice();
+    setComputer(computerChoice);
+    let myResult = judgement(choice[userchoice], computerChoice);
+    setResult(myResult);
+  };
+
+  const judgement = (user, computer) => {
+    if (user.name == computer.name) {
+      return "tie";
+    } else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "win" : "lose";
+    else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "win" : "lose";
+    else if (user.name == "Paper")
+      return computer.name == "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
+  return (
+    <div>
+      <div className="main">
+        <Box title="you" item={select} result={result} />
+        <Box title="computer" item={computer} result={result} />
+      </div>
+      <div className="main">
+        <button onClick={() => play("s")}>가위</button>
+        <button onClick={() => play("r")}>바위</button>
+        <button onClick={() => play("p")}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,32 @@
+import "../App.css";
+import PropTypes from "prop-types";
+
+const Box = (props) => {
+  console.log("props.result", props.result);
+  let result;
+  if (
+    props.title == "computer" &&
+    props.result !== "tie" &&
+    props.result !== null
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
+  return (
+    <div className={`box ${result}`}>
+      <div>{props.title}</div>
+      <img src={props.item && props.item.img} className="item-img" />
+      <h2>{result}</h2>
+    </div>
+  );
+};
+
+Box.propTypes = {
+  title: PropTypes.string.isRequired,
+  item: PropTypes.object,
+  result: PropTypes.string.isRequired,
+};
+
+export default Box;
